d8409d9da778c9afacf3906b868bbf1e00f14538
Diabeloo <99317240+Diabeloo@users.noreply.github.com>
2022-09-18 21:58:11 +0200

    poprawa znakow

--- zad1.cpp
+++ zad1.cpp
@@ -5,17 +5,17 @@ using namespace std;
 int main()
 {
     cin >> a,b;
-    if(x < 7) << cbrt(x) + 1 << cout "wartoœæ funkcji to ", x
+    if(x < 7) << cbrt(x) + 1 << cout "wartosc funkcji to ", x
 }
 else
 {
-    if(x == 7) << cos(x-1) << cout "wartoœæ funkcji to ", x
+    if(x == 7) << cos(x-1) << cout "wartosc funkcji to ", x
 }
 else
 {
-    if(x == 9) << sqrt(3x) << cout "wartoœæ funkcji to ", x
+    if(x == 9) << sqrt(3x) << cout "wartosc funkcji to ", x
 }
 else
 {
-    -8*x << cout "wartoœæ funkcji to ", x
+    -8*x << cout "wartosc funkcji to ", x
 }
