dece89f1ad0f7f63a04a329a4e8472050ff21960
Diabeloo <99317240+Diabeloo@users.noreply.github.com>
2022-09-18 19:18:11 +0200

    zad1

---
+++ zad1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cin >> a,b;
+    if(x < 7) << cbrt(x) + 1 << cout "wartoœæ funkcji to ", x
+}
+else
+{
+    if(x == 7) << cos(x-1) << cout "wartoœæ funkcji to ", x
+}
+else
+{
+    if(x == 9) << sqrt(3x) << cout "wartoœæ funkcji to ", x
+}
+else
+{
+    -8*x << cout "wartoœæ funkcji to ", x
+}
